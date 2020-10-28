17ff4e65aabba4f95f5238345a949204caab2692
Philip Fulcher <philip@nrwl.io>
2020-10-27 21:22:17 -0600

    completed exercise 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,34 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
+function SquaresHistory({squaresHistory = [], currentStep, onStepSelect}) {
+  function handleHistoryChange(index) {
+    onStepSelect(index)
+  }
+
+  return (
+    <ol>
+      {squaresHistory.map((squares, index) => {
+        return (
+          <li key={index}>
+            <button
+              disabled={index === currentStep}
+              onClick={() => handleHistoryChange(index)}
+            >
+              {index === 0 ? 'Go to Game Start' : `Go to move #${index}`}{' '}
+              {index === currentStep ? '(current step)' : ''}
+            </button>
+          </li>
+        )
+      })}
+    </ol>
+  )
+}
+
+function Board({squares, onSquaresChange}) {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -14,28 +38,30 @@ function Board() {
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
 
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
-    //
+    if (winner || squares[square] !== null) {
+      return
+    }
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
-    // 🐨 make a copy of the squares array 
+    // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
-    //
+    const squaresCopy = [...squares]
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
-    //
+    squaresCopy[square] = nextValue
     // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    onSquaresChange(squaresCopy)
   }
 
   function renderSquare(i) {
@@ -49,7 +75,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,18 +91,61 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [
+    squaresHistory,
+    setSquaresHistory,
+  ] = useLocalStorageState('tic-tac-toe:history', [Array(9).fill(null)])
+
+  const [currentStep, setCurrentStep] = useLocalStorageState('currentStep', 0)
+
+  function handleStepSelect(stepId) {
+    setCurrentStep(stepId)
+  }
+
+  function handleSquaresChange(squares) {
+    let newSquaresHistory
+    let nextCurrentStep
+
+    const previousMaxIndex = squaresHistory.length - 1
+
+    if (currentStep === previousMaxIndex) {
+      nextCurrentStep = previousMaxIndex + 1
+      newSquaresHistory = [...squaresHistory, squares]
+    } else {
+      nextCurrentStep = currentStep + 1
+      newSquaresHistory = [...squaresHistory.splice(0, currentStep + 1, 0), squares]
+    }
+    setSquaresHistory(newSquaresHistory)
+    setCurrentStep(nextCurrentStep)
+  }
+
+  function restart() {
+    // 🐨 reset the squares
+    // 💰 `Array(9).fill(null)` will do it!
+    setSquaresHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          squares={squaresHistory[currentStep]}
+          onSquaresChange={handleSquaresChange}
+        />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+        <SquaresHistory
+          currentStep={currentStep}
+          squaresHistory={squaresHistory}
+          onStepSelect={handleStepSelect}
+        />
       </div>
     </div>
   )
